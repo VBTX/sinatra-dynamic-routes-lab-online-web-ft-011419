e8c87c3ab2bafe70b49a151ffac1e4bbb0834a58
Victoria B <viktorija.bourassa@gmail.com>
2019-02-28 06:02:59 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -45,7 +45,7 @@ class App < Sinatra::Base
     num1 = params[:number1].to_i
     num2 = params[:number2].to_i
 
-    result = num1 @symb num2
+    result = "#{num1 @symb num2}"
 
     result
   end
