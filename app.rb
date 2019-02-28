16ed370d0c60374f9b8b8859f40ef9edeaa602cc
Victoria B <viktorija.bourassa@gmail.com>
2019-02-28 06:10:45 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -33,7 +33,6 @@ class App < Sinatra::Base
     @operation = params[:operation]
     num1 = params[:number1].to_i
     num2 = params[:number2].to_i
-    result = ""
     if @operation == "add"
        result = num1 + num2
     elsif @operation == "subtract"
