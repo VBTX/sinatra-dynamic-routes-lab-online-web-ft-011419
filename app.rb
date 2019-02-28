d27815943d3ac5d6c069f902c6bbeb48721abdc3
Victoria B <viktorija.bourassa@gmail.com>
2019-02-28 05:27:18 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,6 +13,11 @@ class App < Sinatra::Base
   end
 
   get '/say/:number/:phrase' do
+    @number = params[:number].to_i
+    @phrase = params[:phrase]
+    @number.times do
+      @phrase
+    end
 
   end
 
